b5a39680d2dacd240c5d1285bf31d53dfd733d2b
jgiorgini <jcgiorgini1971@gmail.com>
2025-11-02 20:52:27 -0300

    Configurar base para GitHub Pages
    Configurar base para GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/stca-dm2-medico-GPT/', // 👈 importante: nombre del repo entre barras
+})
