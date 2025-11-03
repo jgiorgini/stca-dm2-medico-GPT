20535fb2f6ac835e6644caff4a841eef46bb8c83
jgiorgini <jcgiorgini1971@gmail.com>
2025-11-02 21:28:58 -0300

    Fix config formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/stca-dm2-medico-GPT/', // ← importantísimo
+  base: '/stca-dm2-medico-GPT/',
 })
